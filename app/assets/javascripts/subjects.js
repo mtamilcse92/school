b9f59690db73e73b36829e97a82e5e7f3c2c3181
mtamilcse92 <mtamilcse92@gmail.com>
2016-12-02 14:23:15 +0530

    Tamil |with some jquery|

--- app/assets/javascripts/subjects.js
+++ app/assets/javascripts/subjects.js
@@ -1,6 +1,28 @@
 $(document).ready(function(){
     $('.add-button').click(function() {
-  $("#new-form").append($("#new-form").html());
-  console.log($("#new-form:first").html());
+    	var clone = $( ".new-form:first" );
+    	console.log(clone.html());
+  $(this).parent().append($("#new-form").html());
+  // console.log($("#new-form:nth-child(1)").;
 });
 });
+
+ $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
+      starting_top: '4%', // Starting top style attribute
+      ending_top: '10%', // Ending top style attribute
+      ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+        alert("Ready");
+        console.log(modal, trigger);
+      },
+      complete: function() { alert('Closed'); } // Callback for Modal close
+    }
+  );
+
+  $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
